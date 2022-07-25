a03913eca442ca5fb83c387196fd0342e49749be
d1plomatko <merzanz1379@gmail.com>
2022-07-25 22:32:26 +0300

    Initial commit

---
+++ hw1/main.js
@@ -0,0 +1,96 @@
+//Змінні
+
+let greeting = 'hello'
+console.log(greeting)
+alert(greeting)
+document.write(greeting,' ')
+
+let okten = 'owu'
+console.log(okten)
+alert(okten)
+document.write(okten,' ')
+
+let domen = 'com'
+console.log(domen)
+alert(domen)
+document.write(domen,' ')
+
+let region = 'ua'
+console.log(region)
+alert(region)
+document.write(region,' ')
+
+let one = 1
+console.log(one)
+alert(one)
+document.write(one,' ')
+
+let ten = 10
+console.log(ten)
+alert(ten)
+document.write(ten,' ')
+
+let num1 = -999
+console.log(num1)
+alert(num1)
+document.write(num1,' ')
+
+let num2 = 123
+console.log(num2)
+alert(num2)
+document.write(num2,' ')
+
+let pi = 3.14
+console.log(pi)
+alert(pi)
+document.write(pi,' ')
+
+let num3 = 2.7
+console.log(num3)
+alert(num3)
+document.write(num3,' ')
+
+let num4 = 16
+console.log(num4)
+alert(num4)
+document.write(num4,' ')
+
+let bool1 = true
+console.log(bool1)
+alert(bool1)
+document.write(bool1,' ')
+
+let bool2 = false
+console.log(bool2)
+alert(bool2)
+document.write(bool2,' ')
+
+//Конкатенація
+
+let firstName = 'Назар'
+let middleName = 'Петрович'
+let lastName = 'Мерза'
+
+let person = `${firstName} ${middleName} ${lastName}`
+console.log(person)
+
+let a = 100
+let b = '100'
+let c = true;
+
+//typeof
+
+console.log(a, typeof a)
+console.log(b, typeof b)
+console.log(c, typeof c)
+
+//prompt
+
+let fname = prompt('Enter your first name: ')
+console.log(fname)
+
+let mname = prompt('Enter your middle name: ')
+console.log(mname)
+
+let age = +prompt('Enter your age: ')
+console.log(age)
