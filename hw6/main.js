ec0831be54c1f587517e351ebf97030dc3c4f5f8
d1plomatko <merzanz1379@gmail.com>
2022-08-10 10:33:12 +0300

    hw 6

---
+++ hw6/main.js
@@ -0,0 +1,206 @@
+// - Знайти та вивести довижину настипних стрінгових значень
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+
+console.log('hello world'.length, 'lorem ipsum'.length, 'javascript is cool'.length);
+
+// - Перевести до великого регістру наступні стрінгові значення
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+
+console.log('hello world'.toUpperCase(), 'lorem ipsum'.toUpperCase(), 'javascript is cool'.toUpperCase());
+
+// - Перевести до нижнього регістру настипні стрінгові значення
+// 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+
+console.log('HELLO WORLD'.toLowerCase(), 'LOREM IPSUM'.toLowerCase(), 'JAVASCRIPT IS COOL'.toLowerCase());
+
+// - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
+
+let str = ' dirty string   ';
+
+console.log(str.trim());
+
+// - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
+//     let str = 'Ревуть воли як ясла повні';
+// let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']
+
+let str2 = 'Ревуть воли як ясла повні';
+
+let stringToArray = string => string.split(' ');
+let arr = stringToArray(str2);
+console.log(arr);
+
+// - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.
+
+let nums = [10, 8, -7, 55, 987, -1011, 0, 1050, 0];
+
+let numsToString = nums.map(num => num + '');
+
+console.log(numsToString);
+
+// - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
+//     let nums = [11,21,3];
+// sortNums(nums,'ascending') // [3,11,21]
+// sortNums(nums,'descending') // [21,11,3]
+
+let nums2 = [11,21,3];
+
+let sortNums = (array, direction) => {
+    if (direction === 'ascending'){
+        array.sort((a, b) => a - b);
+    }else if (direction === 'descending'){
+        array.sort((a, b) => b - a)
+    }
+    return array
+}
+
+console.log(sortNums(nums2, 'descending'));
+
+
+
+// - є масив
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+
+// -- відсортувати його за спаданням за monthDuration
+
+let sorted = coursesAndDurationArray.sort((course1, course2) => course2.monthDuration - course1.monthDuration)
+
+console.log(sorted);
+// -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+
+let filtered = coursesAndDurationArray.filter(course => course.monthDuration > 5);
+console.log(filtered);
+
+// описати колоду карт
+
+let cards = [
+    {cardSuit: 'clubs', value: 'Ace', color:'black'},
+    {cardSuit: 'clubs', value: 2, color:'black'},
+    {cardSuit: 'clubs', value: 3, color:'black'},
+    {cardSuit: 'clubs', value: 4, color:'black'},
+    {cardSuit: 'clubs', value: 5, color:'black'},
+    {cardSuit: 'clubs', value: 6, color:'black'},
+    {cardSuit: 'clubs', value: 7, color:'black'},
+    {cardSuit: 'clubs', value: 8, color:'black'},
+    {cardSuit: 'clubs', value: 9, color:'black'},
+    {cardSuit: 'clubs', value: 10, color:'black'},
+    {cardSuit: 'clubs', value: 'Jack', color:'black'},
+    {cardSuit: 'clubs', value: 'Queen', color:'black'},
+    {cardSuit: 'clubs', value: 'King', color:'black'},
+
+    {cardSuit: 'diamonds', value: 'Ace', color:'red'},
+    {cardSuit: 'diamonds', value: 2, color:'red'},
+    {cardSuit: 'diamonds', value: 3, color:'red'},
+    {cardSuit: 'diamonds', value: 4, color:'red'},
+    {cardSuit: 'diamonds', value: 5, color:'red'},
+    {cardSuit: 'diamonds', value: 6, color:'red'},
+    {cardSuit: 'diamonds', value: 7, color:'red'},
+    {cardSuit: 'diamonds', value: 8, color:'red'},
+    {cardSuit: 'diamonds', value: 9, color:'red'},
+    {cardSuit: 'diamonds', value: 10, color:'red'},
+    {cardSuit: 'diamonds', value: 'Jack', color:'red'},
+    {cardSuit: 'diamonds', value: 'Queen', color:'red'},
+    {cardSuit: 'diamonds', value: 'King', color:'red'},
+
+    {cardSuit: 'hearts', value: 'Ace', color:'red'},
+    {cardSuit: 'hearts', value: 2, color:'red'},
+    {cardSuit: 'hearts', value: 3, color:'red'},
+    {cardSuit: 'hearts', value: 4, color:'red'},
+    {cardSuit: 'hearts', value: 5, color:'red'},
+    {cardSuit: 'hearts', value: 6, color:'red'},
+    {cardSuit: 'hearts', value: 7, color:'red'},
+    {cardSuit: 'hearts', value: 8, color:'red'},
+    {cardSuit: 'hearts', value: 9, color:'red'},
+    {cardSuit: 'hearts', value: 10, color:'red'},
+    {cardSuit: 'hearts', value: 'Jack', color:'red'},
+    {cardSuit: 'hearts', value: 'Queen', color:'red'},
+    {cardSuit: 'hearts', value: 'King', color:'red'},
+    {value: 'Joker', color:'red'},
+
+    {cardSuit: 'spades', value: 'Ace', color:'black'},
+    {cardSuit: 'spades', value: 2, color:'black'},
+    {cardSuit: 'spades', value: 3, color:'black'},
+    {cardSuit: 'spades', value: 4, color:'black'},
+    {cardSuit: 'spades', value: 5, color:'black'},
+    {cardSuit: 'spades', value: 6, color:'black'},
+    {cardSuit: 'spades', value: 7, color:'black'},
+    {cardSuit: 'spades', value: 8, color:'black'},
+    {cardSuit: 'spades', value: 9, color:'black'},
+    {cardSuit: 'spades', value: 10, color:'black'},
+    {cardSuit: 'spades', value: 'Jack', color:'black'},
+    {cardSuit: 'spades', value: 'Queen', color:'black'},
+    {cardSuit: 'spades', value: 'King', color:'black'},
+    {value: 'Joker', color:'black'},
+
+];
+
+
+// - знайти піковий туз
+
+let aceFinder = cards.find(card => card.cardSuit === 'spades' && card.value === 'Ace');
+
+console.log(aceFinder);
+
+// - всі шістки
+
+let sixFinder = cards.filter(card => card.value === 6);
+console.log(sixFinder);
+
+// - всі червоні карти
+
+let redFinder = cards.filter(card => card.color === 'red');
+console.log(redFinder);
+
+// - всі буби
+
+let clubsFinder = cards.filter(card => card.cardSuit === 'diamonds');
+console.log(clubsFinder);
+
+// - всі трефи від 9 та більше
+
+let filteredClubs = cards.filter(card => card.cardSuit === 'clubs' && card.value >= 9);
+console.log(filteredClubs);
+
+
+//
+//
+// {
+//     cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
+//         value: '', // '6'-'10', 'ace','jack','queen','king','joker'
+//     color:'', // 'red','black'
+// }
+//
+//
+// Додатково по reduce
+// Взяти описану колоду карт, та за допомоги редюсу попакувати всі карти по "мастях" в об'єкт
+// {
+//     spades:[],
+//         diamonds:[],
+//     hearts:[],
+//     clubs:[]
+// }
+
+reducedCards = cards.reduce(function (accumulator, card){
+    if (card.cardSuit === 'spades'){
+        accumulator.spades.push(card);
+    }else if (card.cardSuit === 'diamonds'){
+        accumulator.diamonds.push(card);
+    }else if (card.cardSuit === 'hearts'){
+        accumulator.hearts.push(card);
+    }else if (card.cardSuit === 'clubs'){
+        accumulator.clubs.push(card);
+    }else {
+        accumulator.jokers.push(card);
+    }
+    return accumulator
+
+}, {spades: [], diamonds: [], hearts: [], clubs: [], jokers: []} )
+
+console.log(reducedCards);
